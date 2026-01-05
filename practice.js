e966c7e64fb4922c8f13e42f180081f038ad7672
Md Abdullah Ibn Noor <ab.89noor@gmail.com>
2026-01-05 10:36:02 +0600

    updated code

--- practice.js
+++ practice.js
@@ -1,20 +1,48 @@
-
-// const fetch = require('node-fetch');
-
-// let students = [
-//     { name: "Alice", grade: 85 },
-//     { name: "Bob", grade: 92 },
-//     { name: "Carol", grade: 78 }
-// ];
-
 let person = {
     name: "John Doe",
     age: 30,
     city: "New York"
 }
 
-console.log("Person name is " +person.name);
-console.log(`Person name is ${person.name}`);
+
+console.log(`Person name: ${person.name}`);
+
+let students = [
+    { image: 'www.google.com', name: "Alice", grade: 85 },
+    { name: "Bob", grade: 92 },
+    { name: "Carol", grade: 78 },
+    { name: "David", grade: 88 }
+];
+
+console.log(students[1].name);
+
+students.forEach((student, index)=>{
+    console.log(`Student Name: ${student.name}, Student Grade: ${student.grade}`);
+})
+    // students.forEach(function(student, index){
+
+    // })
+
+// students.forEach((student, index)=>{
+//     console.log(`Student Position ${index + 1}: Person Name: ${student.name}, Grade: ${student.grade}`);
+// });
+
+// // Using Map function
+// let studentNames = students.map(student => student.name);
+// console.log("Student Names:", studentNames);
+
+// let studentGrades = students.map(student => student.grade);
+// console.log("Student Grades:", studentGrades);
+
+// let studentInfo = students.map((student, index) => ({
+//     position: index + 1,
+//     name: student.name,
+//     grade: student.grade
+// }));
+// console.log("Student Info with Position:", studentInfo);
+
+
+
 
 // students.forEach( student => { 
 //     console.log("Student name: " + student.name);
